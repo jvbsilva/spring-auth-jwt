4eda9e78c12b5a47d2246322750877c04c64daef
jvbsilva <joaovictorbarros1@gmail.com>
2024-04-27 20:50:14 -0300

    Inital commit.

---
+++ src/main/java/com/joaovictor/userauthdemo/UserAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictor.userauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovictor/userauthdemo/UserAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictor.userauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
